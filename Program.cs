f78d436883af7205a66e54e6c4578063902d9d45
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-11-21 11:11:18 -0500

    don't need so many objects

--- Program.cs
+++ Program.cs
@@ -12,15 +12,12 @@ namespace NullableReferencesNRE
 
         static async Task Main()
         {
-            object? obj1 = null;
-            object? obj2 = null;
-            object? obj3 = null;
-            object? obj4 = null;
+            object? obj = null;
 
-            object result1 = await Task.FromResult(obj1);                               // Warning
-            object result2 = await ApplyAsync(obj2, x => Task.FromResult(x));           // Warning
-            object result3 = await ApplyAsync<object?, object>(obj3, Task.FromResult);  // Warning
-            object result4 = await ApplyAsync(obj4, Task.FromResult);                   // No warning
+            object result1 = await Task.FromResult(obj);                               // Warning
+            object result2 = await ApplyAsync(obj, x => Task.FromResult(x));           // Warning
+            object result3 = await ApplyAsync<object?, object>(obj, Task.FromResult);  // Warning
+            object result4 = await ApplyAsync(obj, Task.FromResult);                   // No warning
 
             Console.WriteLine(result4.ToString()); // NRE
         }
